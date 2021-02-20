d1c4a3372a5bc662245a00ddc59a08eab52d66d3
Steve McNamara <stephenmcnamara36@gmail.com>
2021-02-19 17:59:08 -0700

    fixed inquirer promises

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const src = require("./src/src");
-
-//code to run init for inquirer and then write to html in output file
+const { Manager, Engineer, Intern } = require("./lib/classes");
 
 function cardGenerator(arr) {
   for (var i = 0; i < arr.length; i++) {
@@ -13,20 +12,74 @@ function cardGenerator(arr) {
     }
   }
 
-  return html + "</body></html>";
+  return (
+    html +
+    `</div>
+          </div>
+      </div>
+  </body>
+  </html>`
+  );
 }
-//first function take in an array of employees and determine which card needs to be generated
-//by the end of the function return html variable with closing tags on them
-//use return html + "</body> </html>"
 
-// normal for loop
-//if getRole() ==="engineer" then call engineerCard()
+function engineerCard() {
+  return `<div class="card border-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header text-white bg-primary">${this.name}</div>
+                <div class="card-body text-primary">
+                    <h5 class="card-title">${this.id}</h5>
+                    <h5 class="card-title"><a href="mailto:${this.email}">${this.email}</a></h5>
+                    <h5 class="card-title"><a href="https://www.github.com/${this.github}">${this.github}</a></h5>
+                </div>
+            </div>`;
+}
 
-//engineerCard()
-//inside will return template literal with html for card
-//internCard()
-//managerCard()
+function internCard() {
+  return `<div class="card border-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header text-white bg-primary">${this.name}</div>
+                <div class="card-body text-primary">
+                    <h5 class="card-title">${this.id}</h5>
+                    <h5 class="card-title"><a href="mailto:${this.email}">${this.email}</a></h5>
+                    <h5 class="card-title">${this.school}</h5>
+                </div>
+            </div>`;
+}
 
-let html = ``;
+function managerCard() {
+  return `<div class="card border-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header text-white bg-primary">${this.name}</div>
+                <div class="card-body text-primary">
+                    <h5 class="card-title">${this.id}</h5>
+                    <h5 class="card-title"><a href="mailto:${this.email}">${this.email}</a></h5>
+                    <h5 class="card-title">${this.officeNumber}</h5>
+                </div>
+            </div>`;
+}
+
+let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="../output/style.css">
+
+    <title>My Team</title>
+</head>
+<body>
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Team!</a>
+        </div>
+    </nav>
+    
+    <!-- As a heading -->
+    <nav class="navbar navbar-custom">
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-2">`;
 
 module.exports = cardGenerator;
--- src/src.js
+++ src/src.js
@@ -1,74 +1,170 @@
-const inquirer = require('inquirer')
-const validator = require('email-validator')
-const { Manager, Engineer, Intern } = require('./lib/classes.js')
-const cardGenerator = require('./index.js')
-const fs = require('fs')
-const path = require ('path')
-const { prompt } = require('inquirer')
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const { prompt } = require("inquirer");
+// const validator = require("email-validator");
+const { Manager, Engineer, Intern } = require("../lib/classes");
+// const cardGenerator = require("../index");
+const fs = require("fs");
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const allEmployees = []
+const allEmployees = [];
 
-//createmanager fn - use inquirer
-    //whenever new employee is created push to allEmployees
 function createManager() {
-    prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the Manager's name?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the Manager's ID number?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the Manager's email?",
-        validate:
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the Manager's office number?",
-      },
-    ]);
-}
-//createintern fn "
-function createIntern() {
+  prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?",
+    },
+  ]).then((answers) => {
+    let manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
 
+    allEmployees.push(manager);
+
+    nextEmployee();
+  });
 }
-//createengineer fn "
+
+function createIntern() {
+  prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email?",
+      // validate: validator,
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What is the intern's school?",
+    },
+  ]).then((answers) => {
+    let intern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+
+    allEmployees.push(intern);
+
+    nextEmployee();
+  });
+}
 
 function createEngineer() {
+  prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email?",
+      // validate: validator.validate("email"),
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's GitHub name?",
+    },
+  ]).then((answers) => {
+    let engineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    );
 
+    allEmployees.push(engineer);
+
+    nextEmployee();
+  });
 }
 
 function nextEmployee() {
-
+  prompt([
+    {
+      type: "list",
+      message: "Would you like to add another employee?",
+      choices: ["Engineer", "Intern", "I'm all finished!"],
+      name: "choices",
+    },
+  ]).then((answers) => {
+    if (answers.choices === "Engineer") {
+      createEngineer();
+    } else if (answers.choices === "Intern") {
+      createIntern();
+    } else {
+      allDone();
+    }
+  });
 }
-//function for what employee do you want to create next, then call one of the above functions - use inquirer
-    // what do you want to do next? make e,m or i or are you done? call done function if done
 
 function allDone() {
-
+  fs.writeFile(outputPath, cardGenerator(allEmployees), (err) => {
+    if (err) throw err;
+    console.log("Success!");
+  });
 }
-// "im done" function that calls function that generates the html
-    //going to look pretty similar to last fn in readme generator
-    //fs.writefile(outputPath, cardGenerator(allEmployees), (err) => {if (err) throw err; console.log(Success!)}) 
-    //const OUTPUT_DIR = path.resolve(__dirname, "output")
-    //const outputPath = path.join(OUTPUT_DIR, "team.html");
-    //
 
+function cardGenerator(arr) {
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++) {
+    if (getRole() === "Engineer") {
+      return html.push(engineerCard());
+    } else if (getRole() === "Manager") {
+      return html.push(managerCard());
+    } else {
+      return html.push(internCard());
+    }
+  }
 
+  return (
+    html +
+    `</div>
+          </div>
+      </div>
+  </body>
+  </html>`
+  );
+}
 
-
-//inquirer prompts with corresponding questions.
-
-
-    prompt([
-            //insert questions here for each class of employee
-    ])
+createManager();
